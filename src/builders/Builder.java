67321083faa74d71d7aecdecc9d4e320fc15091f
= <william.ray@hsc.utah.edu>
2024-09-12 08:34:56 -0600

    create main model builder

---
+++ src/builders/Builder.java
@@ -0,0 +1,14 @@
+package builders;
+
+import repast.simphony.context.Context;
+import repast.simphony.dataLoader.ContextBuilder;
+
+public class Builder implements ContextBuilder<Object> {
+
+	@Override
+	public Context<Object> build(Context<Object> context) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
